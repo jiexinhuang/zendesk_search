9373bc791fd5fdc5b5a1cab93847ea3226f2ef21
Jiexin Huang <jiexin.huang@envato.com>
2017-10-10 00:21:42 +1100

    We have done with model schemas, now something more fun First, let's Make constructor_type less strict, so we can initialize matching model with provided query hash
    Then add #match? method to BaseModel, then we can compare given model
    object with any query hash.
    Also add extra test case for String attribute coercicion and multiple
    key query

--- lib/zendesk_search/models/base_model.rb
+++ lib/zendesk_search/models/base_model.rb
@@ -1,4 +1,12 @@
 module ZendeskSearch
   class BaseModel < Dry::Struct::Value
+    constructor_type :schema
+
+    def match?(query_hash)
+      matching_model = self.class.new(query_hash)
+      query_hash.all? do |key, value|
+        self[key] == matching_model[key]
+      end
+    end
   end
 end
---
+++ spec/models/base_model_spec.rb
@@ -0,0 +1,108 @@
+class TestModel < ZendeskSearch::BaseModel
+  attribute :_id,       ZendeskSearch::Types::Coercible::Int.optional
+  attribute :name,      ZendeskSearch::Types::Coercible::String
+  attribute :disabled,  ZendeskSearch::Types::Form::Bool
+end
+
+RSpec.describe ZendeskSearch::BaseModel do
+  subject(:test_model) { TestModel.new(data) }
+  let(:data) do
+    {
+      _id: id,
+      name: name,
+      disabled: disabled,
+    }
+  end
+  let(:id) { 10 }
+  let(:name) { 'Ronaldo' }
+  let(:disabled) { false }
+
+  describe '#match?' do
+    subject(:match) { test_model.match?(query_hash) }
+
+    context 'when query for id' do
+      context 'and provides the right id(integer attribute)' do
+        let(:query_hash) { { _id: id } }
+        it { is_expected.to eq true }
+      end
+
+      context 'and provides a wrong id' do
+        let(:query_hash) { { _id: 11 } }
+        it { is_expected.to eq false }
+      end
+      
+      context 'and id from data is nil' do
+        let(:id) { nil }
+
+        context 'and provides nil value' do
+          let(:query_hash) { { _id: nil } }
+          it { is_expected.to eq true }
+        end
+      end
+    end
+
+    context 'when query for name' do
+      context 'when provides the right name(string attribute)' do
+        let(:query_hash) { { name: name } }
+        it { is_expected.to eq true }
+      end
+
+      context 'when name from data is empty string' do
+        let(:name) { '' }
+
+        context 'and provides nil value' do
+          let(:query_hash) { { name: nil } }
+          it { is_expected.to eq true }
+        end
+
+        context 'and provides empty string' do
+          let(:query_hash) { { name: '' } }
+          it { is_expected.to eq true }
+        end
+      end
+
+      context 'when name from data is nil' do
+        let(:name) { nil }
+
+        context 'and provides nil value' do
+          let(:query_hash) { { name: nil } }
+          it { is_expected.to eq true }
+        end
+
+        context 'and provides empty string' do
+          let(:query_hash) { { name: '' } }
+          it { is_expected.to eq true }
+        end
+      end
+    end
+
+    context 'when query for disabled state' do
+      context 'and provides the right boolean state' do
+        let(:query_hash) { { disabled: disabled} }
+        it { is_expected.to eq true }
+      end
+
+      context 'and provides the wrong boolean state' do
+        let(:query_hash) { { disabled: true } }
+        it { is_expected.to eq false }
+      end
+    end
+
+    context 'when query for both id and name' do
+      context 'and provides the right values' do
+        let(:query_hash) { { _id: id, name: name } }
+        it { is_expected.to eq true }
+      end
+
+      context 'and provides the wrong id' do
+        let(:query_hash) { { _id: 11, name: name } }
+        it { is_expected.to eq false }
+      end
+
+      context 'and provides the wrong name' do
+        let(:query_hash) { { _id: id, name: 'Messi' } }
+        it { is_expected.to eq false }
+      end
+    end
+  end
+end

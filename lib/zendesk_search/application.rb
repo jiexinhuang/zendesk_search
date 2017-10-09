ae22969bb880eed156005cca5710b0a9dc068304
Jiexin Huang <jiexin.huang@envato.com>
2017-10-10 03:24:39 +1100

    use tty-prompt to build a simple CLI search UI This change just gather search params and send them to data_set

--- lib/zendesk_search/application.rb
+++ lib/zendesk_search/application.rb
@@ -1,8 +1,11 @@
+require 'tty-prompt'
+
 module ZendeskSearch
   class Application
     def initialize(data_file_dir: 'data/', model_classes: ZendeskSearch::MODEL_CLASSES)
       @data_sets = {}
-      model_classes.each do |name, klass|
+      @model_classes = model_classes
+      @model_classes.each do |name, klass|
         json_string = File.read("#{data_file_dir}#{name}.json")
         data_set = Utils::DataImporter.import(
           json_string: json_string,
@@ -10,6 +13,35 @@ module ZendeskSearch
           model_class: klass,
         )
         @data_sets[name] = data_set
+      end
+    end
+
+    def run
+      while(true) do
+        # Select model to query
+        model_name = prompt.select("Choose the model to query", @data_sets.keys)
+        model_class = @model_classes[model_name]
+        attribute_names = model_class.attribute_names
+
+        # Now allow user to multi select attributes to search for
+        selected_attributes = prompt.multi_select("Select attributes to search on #{model_name}", attribute_names, per_page: attribute_names.size)
+
+        # Capture values to search for selected attributes
+        query_hash = {}
+        selected_attributes.each do |attr|
+          search_value = prompt.ask("Please enter search value for #{attr}")
+          query_hash[attr] = search_value
+        end
+
+        # Now use search params to get result
+        results = search(model_name, query_hash)
+        if results.empty?
+          puts 'not result found'
+        else
+          results.each do |row|
+            puts row.to_h
+          end
+        end
       end
     end
 
@@ -22,6 +54,10 @@ module ZendeskSearch
 
     def get_data_set(model_name)
       @data_sets[model_name]
+    end
+
+    def prompt
+      @prompt ||= TTY::Prompt.new
     end
   end
 end

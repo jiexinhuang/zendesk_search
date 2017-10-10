bf26bd84ace70bcd59700539563ef3e04badb6cb
Jiexin Huang <jiexin.huang@envato.com>
2017-10-10 23:46:41 +1100

    add more scenarios in integration test to cover all attribute types

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe ZendeskSearch::Application, type: :integration do
     subject(:search) { application.search(:users, query_hash) }
 
     context 'when searching for records with matching id' do
-      let(:query_hash) { { _id: 1 } }
+      let(:query_hash) { { _id: '1' } }
       it 'returns 1 record with matching id' do
         expect(search.size).to eq 1
         expect(search.first).to be_instance_of(ZendeskSearch::User)
@@ -23,10 +23,30 @@ RSpec.describe ZendeskSearch::Application, type: :integration do
     context 'when searching for records matching locale' do
       let(:query_hash) { { locale: 'zh-CN' } }
 
-      it 'returns record with matching locale' do
+      it 'returns records with matching locale' do
         expect(search.size).to eq 2
         expect(search.first._id).to eq 2
         expect(search.last._id).to eq 5
+      end
+    end
+
+    context 'when searching for records with matching verified bool value' do
+      let(:query_hash) { { verified: 'false' } }
+
+      it 'returns records with matching unverified value' do
+        expect(search.size).to eq 2
+        expect(search.first._id).to eq 3
+        expect(search.last._id).to eq 5
+      end
+    end
+
+    context 'when searching for records with matching tags' do
+      let(:query_hash) { { tags: ['Sutton', 'Gallina'] } }
+
+      it 'returns records with at least one matching tag' do
+        expect(search.size).to eq 2
+        expect(search.first._id).to eq 1
+        expect(search.last._id).to eq 4
       end
     end
   end

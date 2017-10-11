34da9b62cfd84da9772d64ca158ec9357f2f1ee2
Jiexin Huang <jiexin.huang@envato.com>
2017-10-11 11:40:30 +1100

    spike on building in-memory index to speed up search for indexed attributes

--- lib/zendesk_search/models/data_set.rb
+++ lib/zendesk_search/models/data_set.rb
@@ -6,12 +6,49 @@ module ZendeskSearch
       @name = name
       @records = records
       @model_class = model_class
+      @index_attributes = model_class.index
+      index_records
     end
 
     def search(query_hash)
-      matching_model = @model_class.new(query_hash)
-      @records.select do |record|
-        record.match?(matching_model, query_hash.keys)
+      matching_model = model_class.new(query_hash)
+      filtered_record_indexes = filter_records_with_index(query_hash.keys, matching_model)
+      if filtered_record_indexes.nil?
+        records.select do |record|
+          record.match?(matching_model, query_hash.keys)
+        end
+      else
+        filtered_record_indexes.select do |record_index|
+          records[record_index].match?(matching_model, query_hash.keys)
+        end.map do |record_index|
+          records[record_index]
+        end
+      end
+    end
+
+    private
+
+    attr_reader :model_class, :index_attributes
+
+    def filter_records_with_index(query_attributes, matching_model)
+      indexes_to_use = index_attributes & query_attributes
+      if indexes_to_use.any?
+        indexes_to_use.map do |attr|
+          hash_key = matching_model[attr].hash
+          @index[attr][hash_key]
+        end.flatten.uniq
+      end
+    end
+
+    def index_records
+      @index = {}
+      index_attributes.each do |index_attr|
+        @index[index_attr] = {}
+        records.each_with_index do |record, index|
+          hash_key = record[index_attr].hash
+          @index[index_attr][hash_key] = [] if @index[index_attr][hash_key].nil?
+          @index[index_attr][hash_key] << index
+        end
       end
     end
   end
--- spec/models/data_set_spec.rb
+++ spec/models/data_set_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe ZendeskSearch::DataSet do
   let(:record_3) { double }
 
   let(:name) { "User" }
-  let(:model_class) { double(index: []) }
+  let(:model_class) { double(index: [:_id]) }
   subject(:data_set) { ZendeskSearch::DataSet.new(name: name, records: records, model_class: model_class) }
-
+  
+  before do
+    allow(record_1).to receive(:[]).with(:_id).and_return(1)
+    allow(record_2).to receive(:[]).with(:_id).and_return(2)
+    allow(record_3).to receive(:[]).with(:_id).and_return(3)
+  end
 
   describe 'given initialized DataSet' do
     its(:name) { is_expected.to eq name }
@@ -16,17 +21,46 @@ RSpec.describe ZendeskSearch::DataSet do
 
   describe '#search' do
     let(:matching_model) { double }
-    let(:query_hash) { { _id: 1, name: 'John' } }
     subject(:search) { data_set.search(query_hash) }
+
     before do
       allow(model_class).to receive(:new).and_return(matching_model)
-      allow(record_1).to receive(:match?).with(matching_model, [:_id, :name]).and_return(true)
-      allow(record_2).to receive(:match?).with(matching_model, [:_id, :name]).and_return(false)
-      allow(record_3).to receive(:match?).with(matching_model, [:_id, :name]).and_return(true)
+      allow(record_1).to receive(:match?).and_return(true)
+      allow(record_2).to receive(:match?).and_return(false)
+      allow(record_3).to receive(:match?).and_return(true)
     end
 
-    it 'returns records that match with query hash' do
-      expect(search).to eq [record_1, record_3]
+    context 'when searching for unindexed attribute' do
+      let(:query_hash) { { name: 'John' } }
+
+      it 'checks all records and see if they match with query param' do
+        search
+        expect(record_1).to have_received(:match?).with(matching_model, [:name])
+        expect(record_2).to have_received(:match?).with(matching_model, [:name])
+        expect(record_3).to have_received(:match?).with(matching_model, [:name])
+      end
+
+      it 'returns records that match with query hash' do
+        expect(search).to eq [record_1, record_3]
+      end
+    end
+
+    context 'when searching for indexed attribute' do
+      let(:query_hash) { { _id: 1, name: 'John' } }
+      before do
+        allow(matching_model).to receive(:[]).with(:_id).and_return(1)
+      end
+
+      it 'returns records filtered by index and match with query hash' do
+        expect(search).to eq [record_1]
+      end
+
+      it 'only checks records with matching index and see if they match with query param' do
+        search
+        expect(record_1).to have_received(:match?).with(matching_model, [:_id, :name])
+        expect(record_2).not_to have_received(:match?)
+        expect(record_3).not_to have_received(:match?)
+      end
     end
   end
 end
